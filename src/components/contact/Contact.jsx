389e79bc6311cc6951b371d7ccb2dcac68049999
OgutuJoseph <onyango.joseph@yahoo.com>
2022-03-28 22:21:15 +0300

    Theme Context

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,15 +1,19 @@
-import React, { useRef, useState }  from 'react';
+import React, { useRef, useState, useContext }  from 'react';
 import './Contact.css';
 import Phone from '../../images/phone.png';
 import Email from '../../images/email.png';
 import Address from '../../images/address.png';
 import emailjs from '@emailjs/browser';
+import { ThemeContext } from '../../context';
 
 const Contact = () => {
 
     const formRef = useRef();
 
     const [done, setDone] = useState(false);
+
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
 
     const handleSubmit = (e) => {
         //to prevent page refresh
@@ -50,10 +54,10 @@ const Contact = () => {
                         <b>What's your story?</b> Get in touch. Always ready to develop a solution when a new project comes along.
                     </p>  
                     <form ref={formRef} onSubmit={handleSubmit}>
-                        <input type='text' placeholder='Name' name='user_name' /> 
-                        <input type='text' placeholder='Subject' name='user_subject' /> 
-                        <input type='text' placeholder='Email' name='user_email' /> 
-                        <textarea rows='5' placeholder='Message' name='message' />   
+                        <input style={{ backgroundColor: darkMode && '#333' }} type='text' placeholder='Name' name='user_name' /> 
+                        <input style={{ backgroundColor: darkMode && '#333' }} type='text' placeholder='Subject' name='user_subject' /> 
+                        <input style={{ backgroundColor: darkMode && '#333' }} type='text' placeholder='Email' name='user_email' /> 
+                        <textarea style={{ backgroundColor: darkMode && '#333' }} rows='5' placeholder='Message' name='message' />   
                         <button>Submit</button>
                         {done && "Thank you for reaching out to me. Will respond to you soonest!"}
                     </form>  
